f0d319f9dd0456a6421ec7d79e8df4f269010556
Andrew Dicks <adicks@Andrews-MacBook-Pro.local>
2016-02-16 08:27:06 +0000

    Update to also work on the LiveSport project

--- juice.js
+++ juice.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name       JIRA User Interface CSS Edits
 // @namespace  http://corpupsource01.corp.theplatform.com/andrew.dicks/juice
-// @version    0.1.5
+// @version    0.1.6
 // @description  Modifies the stylesheet to improve JIRA UI
 // @include    /https?\:\/\/theplatform\.jira\.com\/.*RapidBoard\.jspa.*/
 // @include    /https?\:\/\/theplatform\.jira\.com\/browse/[A-Z]*-[0-9]*/
+// @include    /https?\:\/\/livesport\.atlassian\.net\/.*RapidBoard\.jspa.*/
+// @include    /https?\:\/\/livesport\.atlassian\.net\/browse/[A-Z]*-[0-9]*/
 // @updateURL  http://corpupsource01.corp.theplatform.com/andrew.dicks/juice/raw/master/juice.js
 // ==/UserScript==
 
